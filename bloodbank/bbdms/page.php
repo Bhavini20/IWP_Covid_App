c633eaa844e8f4273bb31cace89f631d14e5f0a8
NikhithaAlokam <81800759+NikhithaAlokam@users.noreply.github.com>
2021-05-31 23:24:18 +0530

    Update page.php

--- bloodbank/bbdms/page.php
+++ bloodbank/bbdms/page.php
@@ -58,13 +58,13 @@ if($query->rowCount() > 0)
 {
 foreach($results as $result)
 { ?>
-        <h1 class="mt-4 mb-3"><?php   echo htmlentities($result->PageName); ?> </h1>
+        <h1 class="mt-4 mb-3"><?php   echo "Symptoms"; ?> </h1>
 
         <ol class="breadcrumb">
             <li class="breadcrumb-item">
                 <a href="index.php">Home</a>
             </li>
-            <li class="breadcrumb-item active"><?php   echo htmlentities($result->PageName); ?></li>
+            <li class="breadcrumb-item active"><?php   echo "Symptoms" ?></li>
         </ol>
 
         <p><?php  echo $result->detail; ?> </p>
